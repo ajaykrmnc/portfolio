34b16b5fcfd2ef5b736cf76a9d0b79c1be42667b
ajaykrmnc <ajaykg6917@gmail.com>
2023-12-16 14:55:48 +0530

    blog added

--- src/components/MyaddedFile/Blog.js
+++ src/components/MyaddedFile/Blog.js
@@ -4,19 +4,6 @@ import Card from "./BlogCard";
 export default function Project() {
   const githubProject = [
     {
-      id: 0,
-      Name: "6th sem resources",
-      Live: "https://ajaykrmnc.notion.site/91e1d2bc1f4543fd8419105463cce91d?v=1f08156482714d9e989fb8cef3f341c4&pvs=4",
-      intro: "This notion web-page contain all the files and resources that I have gone through during 6th sem at college ",
-      date: "25 Feb 2023"
-    },
-    {
-      id: 3,
-      Name: "VS CODE Debug Setup",
-      Live: "https://ajaykrmnc.notion.site/VS-CODE-c48df183908e4365b47d2a88aedecfb0?pvs=4",
-      intro: "The important file required to do the debug in C++ in VS CODE"
-   },
-    {
       id: 1,
       Live: "https://ajaykrmnc.notion.site/Multisource-BFS-0d24a937ae7a4c6d8c31b22b4f57e828?pvs=4",
       Name: "Multisource BFS",
@@ -27,6 +14,12 @@ export default function Project() {
       Live: "https://bit.ly/ajaydsa",
       Name: "DSA Notes",
       intro: 'DSA notes for Placement Preparation'
+    },
+    {
+      id: 7,
+      Live: "https://ajaykrmnc.notion.site/Segment-Tree-1f2ea3cfa67c497c8c7213060394ec32?pvs=4",
+      Name: "Segment tree templates",
+      intro: 'Segment tree templates for CP and Interview Preparation'
     },
     
     
